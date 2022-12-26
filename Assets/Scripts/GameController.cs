54cca12f8f1f5afbdca95c39f6bea25f1865a60a
irem <iremaydinn.7@gmail.com>
2022-12-26 14:54:38 +0300

    game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(MazeConstructor))]
 public class GameController : MonoBehaviour
 {
+    
+    //1
+    [SerializeField] private FpsMovement player;
+    [SerializeField] private Text timeLabel;
+    [SerializeField] private Text scoreLabel;
+    
+
     private MazeConstructor generator;
+    
+    //2 
+    private DateTime startTime;
+    private int timeLimit;
+    private int reduceLimitBy;
+
+    private int score;
+    private bool goalReached;
+    
 
     void Start()
     {
         generator = GetComponent<MazeConstructor>();
-        generator.GenerateNewMaze(13,15);
+        StartNewGame();
     }
-    
+
+    private void StartNewGame()
+    {
+        timeLimit = 80;
+        reduceLimitBy = 5;
+        startTime = DateTime.Now;
+
+        score = 0;
+        scoreLabel.text = score.ToString();
+        
+        StartNewMaze();
+
+    }
+
+
+
+
+    private void StartNewMaze()
+    {
+        generator.GenerateNewMaze(13,15,OnStartTrigger, OnGoalTrigger);
+
+        float x = generator.startCol * generator.hallWidth;
+        float y = 1;
+        float z = generator.startRow * generator.hallWidth;
+        player.transform.position = new Vector3(x, y, z);
+
+        goalReached = false;
+        player.enabled = true;
+        
+        // restart timer
+
+        timeLimit -= reduceLimitBy;
+        startTime = DateTime.Now;
+    }
+
+
+    private void Update()
+    {
+        if (!player.enabled)
+        {
+            return;
+        }
+
+        int timeUsed = (int)(DateTime.Now - startTime).TotalSeconds;
+        int timeLeft = timeLimit - timeUsed;
+
+        if (timeLeft > 0)
+        {
+            timeLabel.text = timeLeft.ToString();
+        }
+        else
+        {
+            timeLabel.text = "TIME UP";
+            player.enabled = false;
+            
+            Invoke("StartNewGame", 4);
+        }
+    }
+
+    private void OnGoalTrigger(GameObject trigger, GameObject other)
+    {
+        Debug.Log("Goal");
+        goalReached = true;
+
+        score += 1;
+        scoreLabel.text = score.ToString();
+        
+        Destroy(trigger);
+    }
+    private void OnStartTrigger(GameObject trigger, GameObject other)
+    {
+
+        if (!goalReached)
+        {
+            Debug.Log("Finish!");
+            player.enabled = false;
+            
+            Invoke("StartNewMaze", 4);
+        }
+        
+    }
 }
